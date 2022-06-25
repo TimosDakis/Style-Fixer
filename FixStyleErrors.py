f25245cf35771bd8b4312ad083bf2fc2fa8bcf6a
TimosDakis <85649514+TimosDakis@users.noreply.github.com>
2022-06-25 09:39:56 -0400

    Add files via upload

---
+++ FixStyleErrors.py
@@ -0,0 +1,50 @@
+import tkinter.messagebox
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import os
+import re
+
+
+def get_file_name():
+    Tk().withdraw()
+    filename = askopenfilename()
+    if not filename:
+        tkinter.messagebox.showerror(title=None, message="No file picked, exiting")
+        quit()
+    if (os.path.splitext(filename))[1] != ".txt":
+        tkinter.messagebox.showerror(title=None, message="Did not pick a .txt file, exiting")
+        quit()
+    return filename
+
+
+def format_file():
+    searchfilename = get_file_name()
+    formattedfilename = (os.path.splitext(searchfilename))[0] + "Formatted" + ".txt"
+    formattedfile = open(formattedfilename, 'w')
+    searchfile = open(searchfilename, 'r')
+    for line in searchfile:
+        formattedline = regex_style_line(line.rstrip())
+        formattedfile.write(formattedline + "\n")
+    searchfile.close()
+    formattedfile.close()
+
+
+def regex_style_line(line):
+    # fix bullet spacing issues
+    if re.match("(?: {0,7}| {9,})-|(?: {0,3}| {5,})â€¢| +â¬¥", line):
+        line = re.sub("(?: {0,7}| {9,})-", "        -", line)
+        line = re.sub("(?: {0,3}| {5,})â€¢", "    â€¢", line)
+        line = re.sub(" +â¬¥", "â¬¥", line)
+
+    # fix emoji spacing issues
+    line = re.sub("><", "> <", line)
+
+    # fix missing space before/after emoji issues
+    line = re.sub(r"(\w|-|(?:â€¢)|(?:â¬¥))<", r"\g<1> <", line)
+    line = re.sub(r">(\w|-|(?:â€¢)|(?:â¬¥))", r"> \g<1>", line)
+
+    return line
+
+
+if __name__ == '__main__':
+    format_file()
